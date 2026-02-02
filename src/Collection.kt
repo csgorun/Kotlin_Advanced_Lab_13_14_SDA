2f7917e18a62a9c8a4f41de7d12b770befded444
csgorun <dimityi455@gmail.com>
2026-02-02 11:23:22 +0300

    Map collection in Kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -1,17 +1,45 @@
 val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
 val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
-val solarSystem = rockPlanets + gasPlanets
+val solarSystem1 = rockPlanets + gasPlanets
 val newSolarSystem = arrayOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto")
 
 fun main(){
-    val solarSystem = mutableSetOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto")
+    val solarSystem = mutableMapOf(
+        "Mercury" to 0,
+        "Venus" to 0,
+        "Earth" to 1,
+        "Mars" to 2,
+        "Jupiter" to 79,
+        "Saturn" to 82,
+        "Uranus" to 27,
+        "Neptune" to 14
+    )
     println(solarSystem.size)
-    solarSystem.add("Pluton")
+
+    solarSystem["Pluto"] = 5
+
+    println(solarSystem["Pluto"])
+    println(solarSystem["Uranus"])
+    println(solarSystem["Theia"])
+
+    solarSystem.remove("Pluto")
     println(solarSystem.size)
-    println(solarSystem.contains("Pluton"))
-    solarSystem.remove("Pluton")
-    println(solarSystem.size)
-    println(solarSystem.contains("Pluton"))
+
+    solarSystem["Jupiter"] = 82
+    println(solarSystem["Jupiter"])
+
+
+
+
+
+//    val solarSystem = mutableSetOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto")
+//    println(solarSystem.size)
+//    solarSystem.add("Pluton")
+//    println(solarSystem.size)
+//    println(solarSystem.contains("Pluton"))
+//    solarSystem.remove("Pluton")
+//    println(solarSystem.size)
+//    println(solarSystem.contains("Pluton"))
 
 
 
